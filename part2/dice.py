ccef97fa37acd2f235435f25cc9070bcf9e1b7bb
bartingei <johnpaulkibet3@gmail.com>
2025-12-12 13:27:03 +0300

    part 2 added

--- part2/dice.py
+++ part2/dice.py
@@ -66,4 +66,23 @@ dice = []
 total = 0
 num_of_dice = int(input("Hom many dice? "))
 
-ran
+for die in range(num_of_dice):
+    dice.append(random.randint(1,6))
+
+for die in dice:
+    total += die
+
+#for die in range(num_of_dice):
+#    for line in dice_art.get(dice[die]):
+#        print(line)
+
+#to print the die faces on a horizontal line
+
+for line in range(9): #9 here represents 
+    for die in dice:
+        print(dice_art.get(die)[line], end="")
+    print()
+
+print(f"total: {total}")
+
+print(dice)
