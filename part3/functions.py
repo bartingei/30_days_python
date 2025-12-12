cddd0ae9bb94a956683f2ae798415750cfea118b
bartingei <johnpaulkibet3@gmail.com>
2025-12-12 13:26:19 +0300

    learning functions

---
+++ part3/functions.py
@@ -0,0 +1,29 @@
+# functions = a block of reusable code
+#              place () after the function name to invoke it
+
+"""
+
+
+def greet(name):
+    print(f"hello {name}")
+
+greet(name)
+"""
+
+'''name = input("Enter your name ")
+
+def song1():
+    print("Happy birthday to you!")
+
+def song2():
+    print(f"Happy birthday dear {name} ")
+
+song1()
+song1()
+song2()
+song1()'''
+
+def hello(name, age):
+    print(f"hello {name}, you are {age} years old! ")
+
+hello("johnpaul", 12)
