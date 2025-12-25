dbe30a1d91fc71792ee73c710e6baaa9339c197b
bartingei <johnpaulkibet3@gmail.com>
2025-12-25 17:16:35 +0300

    encryption practise

--- part5/psh.py
+++ part5/psh.py
@@ -21,6 +21,22 @@ for letter in value:
 	index = value.index(letter)
 	encryptedtxt += key[index]
 
-print(f" original: {value}")
-print(f" encrypted text: {encryptedtxt}")
+#print(f" original: {value}")
+#print(f" encrypted text: {encryptedtxt}")
+
+print("-" * 10)
+print("your text has been successfully encrypted")
+print("-" * 10)
+# decryption
+decryptedtxt = ""
+
+for j in encryptedtxt:
+	index = encryptedtxt.index(j)
+	decryptedtxt += value[index]
+
+print("-" * 10)
+print(" decryption successful")
+print("-" * 10)
+print(f" ENCRYPTED TEXT: {encryptedtxt}")
+#print(f" decrypted text : {decryptedtxt}")
 
