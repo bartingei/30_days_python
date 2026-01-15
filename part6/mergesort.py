8afd0fc823f240ce4390bcf3de405816ce10f4fe
bartingei <johnpaulkibet3@gmail.com>
2026-01-15 18:51:27 +0300

    merge sort

---
+++ part6/mergesort.py
@@ -0,0 +1,33 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+
+    return merge(left_half, right_half)
+
+
+def merge(left, right):
+    sorted_arr = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            sorted_arr.append(left[i])
+            i += 1
+        else:
+            sorted_arr.append(right[j])
+            j += 1
+
+    # Append leftovers
+    sorted_arr.extend(left[i:])
+    sorted_arr.extend(right[j:])
+
+    return sorted_arr
+
+
+numbers = [87, 20, 100, 43, 222, 6, 12, 98, 33]
+print(merge_sort(numbers))
+
